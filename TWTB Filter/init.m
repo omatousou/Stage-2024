2afe3be792a4ee9db624368722a624b80094eee9
omatousou <79108301+omatousou@users.noreply.github.com>
2024-07-12 14:02:56 +0200

    Delete TWTB Filter directory

--- TWTB Filter/init.m
+++
@@ -1,8 +0,0 @@
-function [rawdata, rawWL, AOI] = init()
-    load("DataL057B.mat")
-
-    % get data
-    rawdata = SemrockFilters.TBP0156114x25x36.Data;
-    rawWL = SemrockFilters.TBP0156114x25x36.WL;
-    AOI = SemrockFilters.TBP0156114x25x36.AOI;
-end
